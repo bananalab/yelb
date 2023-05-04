5634a98dd3a6a660a71a26d240b0a3d2241d8e8b
Robert Jordan <39421615+rojopolis@users.noreply.github.com>
2023-05-04 22:29:49 +0000

    Frontend build.

--- yelb-ui/build.sh
+++ yelb-ui/build.sh
@@ -1,24 +1,26 @@
 #!/bin/bash -uex
 npm install -g @angular/cli@6.0.0
-npm install node-sass
+npm install node-sass@4.13.1 --legacy-peer-deps
 
 git clone https://github.com/vmware/clarity-seed.git
 cd clarity-seed
 git checkout -b f3250ee26ceb847f61bb167a90dc957edf6e7f43
 
-cp ../clarity-seed-newfiles/src/index.html /clarity-seed/src/index.html
-cp ../clarity-seed-newfiles/src/styles.css /clarity-seed/src/styles.css
-cp ../clarity-seed-newfiles/src/env.js /clarity-seed/src/env.js
-cp ../clarity-seed-newfiles/src/app/app* /clarity-seed/src/app
-cp ../clarity-seed-newfiles/src/app/env* /clarity-seed/src/app
-cp ../clarity-seed-newfiles/src/environments/env* /clarity-seed/src/environments
-cp ../clarity-seed-newfiles/package.json /clarity-seed/package.json
-cp ../clarity-seed-newfiles/angular-cli.json /clarity-seed/.angular-cli.json
-rm -r ../clarity-seed/src/app/home
-rm -r ../clarity-seed/src/app/about
+cp ../clarity-seed-newfiles/src/index.html src/index.html
+cp ../clarity-seed-newfiles/src/styles.css src/styles.css
+cp ../clarity-seed-newfiles/src/env.js src/env.js
+cp ../clarity-seed-newfiles/src/app/app* src/app
+cp ../clarity-seed-newfiles/src/app/env* src/app
+cp ../clarity-seed-newfiles/src/environments/env* src/environments
+cp ../clarity-seed-newfiles/package.json package.json
+cp ../clarity-seed-newfiles/angular-cli.json .angular-cli.json
+rm -r src/app/home
+rm -r src/app/about
 
 cd src
 
 npm install  
 
-ng build --environment=prod --output-path=dist/ -aot -vc -cc -dop --buildOptimizer
+ng build --environment=prod --output-path=./prod/dist/ -aot -vc -cc -dop --buildOptimizer
+ng build --environment=test --output-path=./test/dist/
+ng build --environment=dev --output-path=./dev/dist/
